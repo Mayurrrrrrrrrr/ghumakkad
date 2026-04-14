6db29ce90e1b837e8876024ff4e78f5abc53ba62
Mayurrrrrrrrrr <mayurdubey2003@gmail.com>
2026-04-14 08:35:55 +0530

    fix: database credentials for prod

--- backend/lib/src/config/database.dart
+++ backend/lib/src/config/database.dart
@@ -1,7 +1,7 @@
 class DbConfig {
   static const String host = 'localhost';
   static const int port = 3306;
-  static const String user = 'ghumakkad_user';
-  static const String password = 'YOUR_PASSWORD_HERE';
+  static const String user = 'mayur_user';
+  static const String password = 'Mayur@12345';
   static const String database = 'ghumakkad_db';
 }
